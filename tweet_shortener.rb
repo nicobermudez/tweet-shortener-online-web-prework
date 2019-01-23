b575a4754794d5bf741cc58b074d04b67bdd8e54
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 16:09:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  word_substituter(tweet).length > 140 ? tweet[0..140] : tweet
+  word_substituter(tweet).length > 140 ? tweet[0..139] : tweet
 end
 
 
