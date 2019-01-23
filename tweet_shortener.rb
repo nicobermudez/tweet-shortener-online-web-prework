87f728f0dd03dcdb085493e46e0c32326b06e8b3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 16:01:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ def dictionary
 end
 
 def word_substituter(string)
-  string.split.collect do |word|
+  array = string.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
