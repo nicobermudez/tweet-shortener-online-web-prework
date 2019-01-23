18f42c632aace22f664e01d1974cbcf30faf4457
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 15:27:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,13 @@
 # Write your code here.
 def dictionary
-  "hello" becomes 'hi'
-  "to, two, too" become '2' 
-  "for, four" become '4'
-  'be' becomes 'b'
-  'you' becomes 'u'
-  "at" becomes "@" 
-  "and" becomes "&"
+  dictionary = {
+    "hello": 'hi', 
+    "to, two, too" => '2',
+    "for, four" => '4'
+    'be' => 'b'
+    'you' => 'u'
+    "at" => "@" 
+    "and" => "&"}
 end
 
 def word_substituter(string)
